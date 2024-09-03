a305c3c92f96cf3006d85135fe4897f707ac9d2c
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2024-09-02 22:14:09 -0500

    Adición de los componentes para mejorar la interfaz de la cabecera

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header-contents">
+                <h2>Ordene su comida favorita aquí.</h2>
+                <p>Elija de un diverso menu con una colección deleitable de platos creados con los mejores ingredientes y experiencia culinaria. Nuestra misión es satisfacer su gusto y elevar su experiencia de cena, un delicioso plato a la vez.</p>
+                <button>Ver Menú</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
